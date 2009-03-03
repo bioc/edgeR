0476582ef89b33622074df7156c7f913fd852723
Mark Robinson <mrobinson@wehi.edu.au>
2009-03-03 06:42:43 +0000

    fixed bug in topTags -- A was calculated incorrectly
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_3/madman/Rpacks/edgeR@37682 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/topTags.R
+++ R/topTags.R
@@ -2,7 +2,7 @@
 topTags<-function(object,n=10,adj.method= "BH") {
   if (!is(object,"deDGEList"))
     stop("Currently only supports deDGEList objects.")
-  tab<-data.frame(A=log2(object$ps$p1)+log2(object$ps$p2)/2,M=log2(object$ps$p2/object$ps$p1),P.Value=object$exact,adj.P.Val=p.adjust(object$exact,adj.method))
+  tab<-data.frame(A=(log2(object$ps$p1)+log2(object$ps$p2))/2,M=log2(object$ps$p2/object$ps$p1),P.Value=object$exact,adj.P.Val=p.adjust(object$exact,adj.method))
   rownames(tab)<-rownames(object$pseudo)
   o<-order(tab$P.Value)
   tab[o[1:min(nrow(tab),n)],]
