86c98c8a52312750d6408211510d77eb79aba0a3
Mark Robinson <mrobinson@wehi.edu.au>
2009-07-26 13:43:33 +0000

    modifications to plotMA for those points with 0-in-1-group
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_4/madman/Rpacks/edgeR@40848 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotMA.R
+++ R/plotMA.R
@@ -1,10 +1,13 @@
 # Written by Mark Robinson, edited by Davis McCarthy, February 2009
 # A function/method to plot differential expression against overall expression (equivalent to plots used in microarray analyis)
-plotMA<-function(object,pair=c(1,2),xlab="A",ylab="M",ylim=NULL,pch=19,...) {
+plotMA<-function(object,pair=c(1,2),xlab="A",ylab="M",ylim=NULL,pch=19, eps=0, smearOffset=0, ...) {
 	g1<-pair[1]
 	g2<-pair[2]
-	M<-log2(object$ps$p.group[,g2]/object$ps$p.group[,g1])
+	M<-log2( (object$ps$p.group[,g2]+eps)/(object$ps$p.group[,g1]+eps))
 	A<-(log2(object$ps$p.group[,g1])+log2(object$ps$p.group[,g2]))/2
+        m <- min(object$ps$p.group)
+        w <- object$ps$p.group[,g1]==m | object$ps$p.group[,g2]==m
+        A[w] <- min(A[!w])+runif(sum(w))+smearOffset
 	if(is.null(ylim))
 		ylim<-c(-1,1)*max(abs(M))
 	plot( A, M, pch=pch, xlab=xlab,ylab=ylab,ylim=ylim,...)
