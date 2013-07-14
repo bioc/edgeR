3296f685dde6d13914da5a4b13ff88a0b9623bf2
Gordon Smyth <smyth@wehi.edu.au>
2013-07-14 06:43:31 +0000

    Version 3.2.4 -- aligning with devel version. - Refinement to cutWithMinN() to make the bin numbers more equal in the worst case. - estimateDisp() now creates the design matrix correctly when the design matrix is not given as an argument and there is only one group.  Previously this case gave an error. - plotMDS.DGEList now gives a friendly error message when there are fewer than 3 data columns. - Refinement to computation for nbins in dispBinTrend.  Now changes more smoothly with the number of genes.  trace argument is retired. - Fixes to calcNormFactors with method="TMM" so that it takes account of lib.size and refCol if these are preset. - Some fixes and cleaning up of subsetting code. - Minor edit to glm.h code. - Edits to camera.DGEList, cutWithMinN and plotMDS.DGEList help pages.  Edits to help pages for the data classes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_12/madman/Rpacks/edgeR@78429 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/dispBinTrend.R
+++ R/dispBinTrend.R
@@ -1,8 +1,8 @@
-dispBinTrend <- function(y, design=NULL, offset=NULL, df=5, span=0.3, min.n=400, method.bin="CoxReid", method.trend="spline", trace=0, AveLogCPM=NULL, ...)
+dispBinTrend <- function(y, design=NULL, offset=NULL, df=5, span=0.3, min.n=400, method.bin="CoxReid", method.trend="spline", AveLogCPM=NULL, ...)
 #	Estimate common dispersion in bins based on AveLogCPM,
 #	then fit a curve through the dispersions
 #	Davis McCarthy, Gordon Smyth
-#	Created 10 Feb 2011.  Last modified 11 March 2013.
+#	Created 10 Feb 2011.  Last modified 17 April 2013.
 {
 #	Check y
 	y <- as.matrix(y)
@@ -32,8 +32,22 @@ dispBinTrend <- function(y, design=NULL, offset=NULL, df=5, span=0.3, min.n=400,
 #	Define bins of genes based on min.n in each bin
 #	All zero rows are marked as group==0
 	group <- as.numeric(pos)
-	nbins <- floor(npostags/min.n)
-	nbins <- min(max(nbins,1),1000)
+
+	if(npostags < 100)
+		nbins <- 1
+	else {
+		nbins <- floor(npostags^0.4)
+		nbins <- min(nbins,1000)
+		min.n <- min(min.n,floor(npostags/nbins))
+	}
+	if(min.n < 50) {
+		nbins <- floor(npostags/50)
+		min.n <- 50
+	}
+
+#	nbins <- floor(npostags/min.n)
+#	nbins <- min(max(nbins,1),1000)
+
 	if(nbins>1) group[pos] <- cutWithMinN(AveLogCPM[pos],intervals=nbins,min.n=min.n)$group
 
 #	Estimate dispersion in each bin
