1144355dc3c8b555bbfe1544215f61e9e7fecf0b
Gordon Smyth <smyth@wehi.edu.au>
2019-04-23 13:27:27 +1000

    edger 3.25.5 - TMMwzp renamed to TMMwsp - TMMwsp promoted the default method for calcNormFactors

--- R/calcNormFactors.R
+++ R/calcNormFactors.R
@@ -1,7 +1,7 @@
 calcNormFactors <- function(object, ...)
 UseMethod("calcNormFactors")
 
-calcNormFactors.DGEList <- function(object, method=c("TMM","TMMwzp","RLE","upperquartile","none"), refColumn=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10, p=0.75, ...)
+calcNormFactors.DGEList <- function(object, method=c("TMMwsp","TMM","RLE","upperquartile","none"), refColumn=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10, p=0.75, ...)
 #	Scale normalization of RNA-Seq data, for DGEList objects
 #	Created 2 October 2014.  Last modified 27 August 2015.
 {
@@ -9,11 +9,11 @@ calcNormFactors.DGEList <- function(object, method=c("TMM","TMMwzp","RLE","upper
 	object
 }
 
-calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwzp","RLE","upperquartile","none"), refColumn=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10, p=0.75, ...)
+calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMMwsp","TMM","RLE","upperquartile","none"), refColumn=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10, p=0.75, ...)
 #	Scale normalization of RNA-Seq data, for count matrices
 #	Mark Robinson.  Edits by Gordon Smyth.
 #	Created October 22 October 2009 by Mark Robinson.
-#	Last modified 23 Sep 2018.
+#	Last modified 23 Apr 2019.
 {
 #	Check object
 	x <- as.matrix(object)
@@ -24,6 +24,8 @@ calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwz
 	if(any(is.na(lib.size))) stop("NA lib.sizes not permitted")
 
 #	Check method
+#	Backward compatability with previous name
+	if(length(method)==1L && method=="TMMwzp") method="TMMwsp"
 	method <- match.arg(method)
 
 #	Remove all zero rows
@@ -41,16 +43,16 @@ calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwz
 			if(length(refColumn)==0 | refColumn < 1 | refColumn > ncol(x)) refColumn <- 1
 			f <- rep(NA,ncol(x))
 			for(i in 1:ncol(x))
-				f[i] <- .calcFactorWeighted(obs=x[,i],ref=x[,refColumn], libsize.obs=lib.size[i], libsize.ref=lib.size[refColumn], logratioTrim=logratioTrim, sumTrim=sumTrim, doWeighting=doWeighting, Acutoff=Acutoff)
+				f[i] <- .calcFactorTMM(obs=x[,i],ref=x[,refColumn], libsize.obs=lib.size[i], libsize.ref=lib.size[refColumn], logratioTrim=logratioTrim, sumTrim=sumTrim, doWeighting=doWeighting, Acutoff=Acutoff)
 			f
 		},
-		TMMwzp = {
+		TMMwsp = {
 			f75 <- .calcFactorQuantile(data=x, lib.size=lib.size, p=0.75)
 			if( is.null(refColumn) ) refColumn <- which.min(abs(f75-mean(f75)))
 			if(length(refColumn)==0 | refColumn < 1 | refColumn > ncol(x)) refColumn <- 1
 			f <- rep(NA,ncol(x))
 			for(i in 1:ncol(x))
-				f[i] <- .calcFactorTMMwzp(obs=x[,i],ref=x[,refColumn], libsize.obs=lib.size[i], libsize.ref=lib.size[refColumn], logratioTrim=logratioTrim, sumTrim=sumTrim, doWeighting=doWeighting, Acutoff=Acutoff)
+				f[i] <- .calcFactorTMMwsp(obs=x[,i],ref=x[,refColumn], libsize.obs=lib.size[i], libsize.ref=lib.size[refColumn], logratioTrim=logratioTrim, sumTrim=sumTrim, doWeighting=doWeighting, Acutoff=Acutoff)
 			f
 		},
 		RLE = .calcFactorRLE(x)/lib.size,
@@ -64,7 +66,6 @@ calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwz
 #	Output
 	f
 }
-
 
 .calcFactorRLE <- function (data)
 {
@@ -81,7 +82,7 @@ calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwz
 #	f/exp(mean(log(f)))
 }
 
-.calcFactorWeighted <- function(obs, ref, libsize.obs=NULL, libsize.ref=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10)
+.calcFactorTMM <- function(obs, ref, libsize.obs=NULL, libsize.ref=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10)
 #	TMM between two libraries
 {
 	obs <- as.numeric(obs)
@@ -126,20 +127,94 @@ calcNormFactors.default <- function(object, lib.size=NULL, method=c("TMM","TMMwz
 	2^f
 }
 
-.calcFactorTMMwzp <- function(obs, ref, libsize.obs=NULL, libsize.ref=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10)
-#	TMM with "pairing of zeros" between the two libraries
+.calcFactorTMMwsp <- function(obs, ref, libsize.obs=NULL, libsize.ref=NULL, logratioTrim=.3, sumTrim=0.05, doWeighting=TRUE, Acutoff=-1e10)
+#	TMM with pairing of singleton positive counts between the obs and ref libraries
 #	Gordon Smyth
-#	19 Sep 2018
+#	Created 19 Sep 2018. Last modified 23 April 2019.
 {
+	obs <- as.numeric(obs)
+	ref <- as.numeric(ref)
+
+#	epsilon serves as floating-point zero
 	eps <- 1e-14
-	i <- (obs < eps) & (ref >= eps)
-	j <- (obs >= eps) & (ref < eps)
-	minij <- min(sum(i),sum(j))
-	if(minij==0L) return(.calcFactorWeighted(obs=obs,ref=ref,libsize.obs=libsize.obs,libsize.ref=libsize.ref,logratioTrim=logratioTrim,sumTrim=sumTrim,doWeighting=doWeighting,Acutoff=Acutoff))
-	k <- i | j
-	refk <- sort(ref[k],decreasing=TRUE)
-	obsk <- sort(obs[k],decreasing=TRUE)
-	obs <- c(obs[!k],obsk)
-	ref <- c(ref[!k],refk)
-	.calcFactorWeighted(obs=obs,ref=ref,libsize.obs=libsize.obs,libsize.ref=libsize.ref,logratioTrim=logratioTrim,sumTrim=sumTrim,doWeighting=doWeighting,Acutoff=Acutoff)
+
+#	Identify zero counts
+	pos.obs <- (obs > eps)
+	pos.ref <- (ref > eps)
+	npos <- 2L * pos.obs + pos.ref
+
+#	Remove double zeros and NAs
+	i <- which(npos==0L | is.na(npos))
+	if(length(i)) {
+		obs <- obs[-i]
+		ref <- ref[-i]
+		npos <- npos[-i]
+	}
+
+#	Check library sizes	
+	if(is.null(libsize.obs)) libsize.obs <- sum(obs)
+	if(is.null(libsize.ref)) libsize.ref <- sum(ref)
+
+#	Pair up as many singleton positives as possible
+#	The unpaired singleton positives are discarded so that no zeros remain
+	zero.obs <- (npos == 1L)
+	zero.ref <- (npos == 2L)
+	k <- (zero.obs | zero.ref)
+	n.eligible.singles <- min( sum(zero.obs), sum(zero.ref))
+	if(n.eligible.singles > 0L) {
+		refk <- sort(ref[k],decreasing=TRUE)[1:n.eligible.singles]
+		obsk <- sort(obs[k],decreasing=TRUE)[1:n.eligible.singles]
+		obs <- c(obs[!k],obsk)
+		ref <- c(ref[!k],refk)
+	} else {
+		obs <- obs[!k]
+		ref <- ref[!k]
+	}
+
+#	Any left?
+	n <- length(obs)
+	if(n==0L) return(1)
+
+#	Compute M and A values
+	obs.p <- obs / libsize.obs
+	ref.p <- ref / libsize.ref
+	M <- log2( obs.p / ref.p )
+	A <- 0.5 * log2( obs.p * ref.p )
+
+#	If M all zero, return 1
+	if(max(abs(M)) < 1e-6) return(1)
+
+#	M order, breaking ties by shrunk M
+	obs.p.shrunk <- (obs+0.5) / (libsize.obs+0.5)
+	ref.p.shrunk <- (ref+0.5) / (libsize.ref+0.5)
+	M.shrunk <- log2( obs.p.shrunk / ref.p.shrunk )
+	o.M <- order(M, M.shrunk)
+
+#	A order
+	o.A <- order(A)
+
+#	Trim
+	loM <- as.integer(n * logratioTrim) + 1L
+	hiM <- n + 1L - loM
+	keep.M <- rep(FALSE,n)
+	keep.M[o.M[loM:hiM]] <- TRUE
+	loA <- as.integer(n * sumTrim) + 1L
+	hiA <- n + 1L - loA
+	keep.A <- rep(FALSE,n)
+	keep.A[o.A[loA:hiA]] <- TRUE
+	keep <- keep.M & keep.A
+	M <- M[keep]
+
+#	Average the M values
+	if(doWeighting) {
+		obs.p <- obs.p[keep]
+		ref.p <- ref.p[keep]
+		v <- (1-obs.p)/obs.p/libsize.obs + (1-ref.p)/ref.p/libsize.ref
+		w <- (1+1e-6) / (v+1e-6)
+		TMM <- sum(w*M) / sum(w)
+	} else {
+		TMM <- mean(M)
+	}
+
+	2^TMM
 }
