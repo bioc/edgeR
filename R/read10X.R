4e03673b8427eef28ba0424069758e5c9e205efc
Yunshun Chen <yuchen@wehi.edu.au>
2019-04-24 16:02:49 +1000

    edgeR 3.25.7 - Update read10X() to automatically detect files names from latest CellRanger version; - Update NEWS.Rd; - Update user's guide.

--- R/read10X.R
+++ R/read10X.R
@@ -1,22 +1,45 @@
-read10X <- function(mtx="matrix.mtx",genes="genes.tsv",barcodes=NULL,path=NULL,DGEList=TRUE)
+read10X <- function(mtx=NULL,genes=NULL,barcodes=NULL,path=".",DGEList=TRUE)
+#	Read 10X Genomics Matrix Exchange Format files created by CellRanger
+#	Gordon Smyth
+#	Created 10 Jan 2018. Last modified 24 Apr 2019.
 {
-#	Add path
-	if(!is.null(path)) {
-		mtx <- file.path(path,mtx)
-		genes <- file.path(path,genes)
-		if(!is.null(barcodes)) barcodes <- file.path(path,barcodes)
+#	Get file names
+	if(is.null(mtx) || is.null(genes) || is.null(barcodes)) {
+		files <- dir(path)
+		if(is.null(mtx)) {
+			if("matrix.mtx.gz" %in% files) mtx <- "matrix.mtx.gz"
+			if("matrix.mtx" %in% files) mtx <- "matrix.mtx"
+			if(is.null(mtx)) stop("Can't find matrix.mtx file, please specify filename explicitly")
+		}
+		if(is.null(genes)) {
+			if("genes.tsv.gz" %in% files) genes <- "genes.tsv.gz"
+			if("genes.tsv" %in% files) genes <- "genes.tsv"
+			if("features.tsv.gz" %in% files) genes <- "features.tsv.gz"
+			if("features.tsv" %in% files) genes <- "features.tsv"
+			if(is.null(genes)) stop("Can't find genes.tsv or features.tsv, please specify filename explicitly")
+		}
+		if(is.null(barcodes)) {
+			if("barcodes.tsv" %in% files) barcodes <- "barcodes.tsv"
+			if("barcodes.tsv.gz" %in% files) barcodes <- "barcodes.tsv.gz"
+		}
 	}
 
+#	Add path
+	mtx <- file.path(path,mtx)
+	genes <- file.path(path,genes)
+	if(!is.null(barcodes)) barcodes <- file.path(path,barcodes)
+
 #	Fetch header info for checking
-	N <- scan(mtx,skip=2,what=0L,sep=" ",n=3,quiet=TRUE)
+	N <- scan(mtx,skip=2,what=0L,sep=" ",nmax=3,quiet=TRUE)
 	ngenes <- N[1]
 	ncells <- N[2]
 	nmtx <- N[3]
 
 #	Read gene Ids
 	Genes <- read.table(genes,header=FALSE,comment.char="",sep="\t",row.names=1,colClasses="character")
-	names(Genes) <- "Symbol"
-	if(nrow(Genes) != ngenes) stop("Number of gene IDs doesn't agree with header information in mtx file")
+	if(nrow(Genes) != ngenes) stop("Number of feature IDs doesn't agree with header information in mtx file")
+	names(Genes)[1] <- "Symbol"
+	if(ncol(Genes) > 1L) names(Genes)[2] <- "Type"
 
 #	Read mtx file of counts
 	m <- read.table(mtx,skip=3,header=FALSE,comment.char="",sep=" ",colClasses="integer",nrows=nmtx)
